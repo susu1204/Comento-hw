309d1093886a29f2bef110830cf53b05c0576632
subin <whtnqls03@naver.com>
2023-11-16 21:24:45 +0900

    displayResult() 함수 제거

--- 3week/#2calculator/3week_hw_2.js
+++ 3week/#2calculator/3week_hw_2.js
@@ -15,9 +15,8 @@ buttons.forEach(button => {
                 const expression = display.textContent
                     .replace(/×/g, '*')
                     .replace(/÷/g, '/');
-                const result = evaluate(expression);
-                displayResult(result);
-                updateFontSize();
+                display.textContent = evaluate(expression);
+                // JavaScript에서는 정확한 20자리 이상의 연산을 지원하지 않으므로 결과값이 지수표기법으로 나오도록 유지..ㅠ
             } catch (error) {
                 display.textContent = 'Error';
             }
@@ -26,20 +25,13 @@ buttons.forEach(button => {
                 display.textContent = buttonText;
             } else {
                 const newContent = display.textContent + buttonText;
+                
                 display.textContent = newContent;
                 updateFontSize();
             }
         }
     });
 });
-
-// function displayResult(result) {
-//     const resultString = result.toLocaleString('fullwide', {useGrouping: false});
-//     display.textContent = resultString;
-// }
-function displayResult(result) {
-    display.textContent = formatNumber(result);
-}
 
 function updateFontSize() {
     const currentFontSize = parseInt(window.getComputedStyle(display).fontSize);
@@ -54,10 +46,3 @@ function resetDisplayStyle() {
     display.style.fontSize = '28px';
 }
 
-function formatNumber(number) {
-    // 숫자를 지수 표기법으로 변환하지 않고 포맷
-    const formatter = new Intl.NumberFormat('en-US', {
-        maximumFractionDigits: 20
-    });
-    return formatter.format(number);
-}
