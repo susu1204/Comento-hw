e89afc50bc575826d1a18dc740157f0a4b53094b
subin <whtnqls03@naver.com>
2023-11-16 21:02:56 +0900

    계산 결과가 지수 표기법으로 나오지 않게 함수 추가

--- 3week/#2calculator/3week_hw_2.js
+++ 3week/#2calculator/3week_hw_2.js
@@ -15,7 +15,9 @@ buttons.forEach(button => {
                 const expression = display.textContent
                     .replace(/×/g, '*')
                     .replace(/÷/g, '/');
-                display.textContent = evaluate(expression);
+                const result = evaluate(expression);
+                displayResult(result);
+                updateFontSize();
             } catch (error) {
                 display.textContent = 'Error';
             }
@@ -24,7 +26,6 @@ buttons.forEach(button => {
                 display.textContent = buttonText;
             } else {
                 const newContent = display.textContent + buttonText;
-                
                 display.textContent = newContent;
                 updateFontSize();
             }
@@ -32,9 +33,14 @@ buttons.forEach(button => {
     });
 });
 
+function displayResult(result) {
+    const resultString = result.toLocaleString('fullwide', { useGrouping: false });
+    display.textContent = resultString;
+}
+
 function updateFontSize() {
     const currentFontSize = parseInt(window.getComputedStyle(display).fontSize);
-    const maxCharacters = Math.floor((display.offsetWidth - 10) / (currentFontSize * 0.6)); 
+    const maxCharacters = Math.floor((display.offsetWidth - 10) / (currentFontSize * 0.6));
     if (display.textContent.length > maxCharacters) {
         display.style.fontSize = `${currentFontSize * 0.9}px`;
     }
@@ -44,4 +50,3 @@ function resetDisplayStyle() {
     display.textContent = '0';
     display.style.fontSize = '28px';
 }
-
