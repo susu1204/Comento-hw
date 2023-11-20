70149d13deddc583441aa553a9482c0fbe0d588b
subin <whtnqls03@naver.com>
2023-11-20 23:30:00 +0900

    js 함수 분리 및 양음수 변환 기능 추가

--- 3week/#2calculator/3week_hw_2.js
+++ 3week/#2calculator/3week_hw_2.js
@@ -1,38 +1,75 @@
 const display = document.getElementById('display');
 const buttons = document.querySelectorAll('.btn');
+const negateButton = document.getElementById('negateBtn');
 
+// 수식 평가 함수
 const evaluate = expression => {
-    return Function('"use strict";return (' + expression + ')')();
+    return Function('"use strict"; return (' + expression + ')')();
 };
 
+// 버튼 클릭 이벤트 핸들러 등록
 buttons.forEach(button => {
     button.addEventListener('click', function() {
         const buttonText = button.textContent;
-        if (buttonText === 'C') {
-            resetDisplayStyle();
-        } else if (buttonText === '=') {
-            try {
-                const expression = display.textContent
-                    .replace(/×/g, '*')
-                    .replace(/÷/g, '/');
-                display.textContent = evaluate(expression);
-                // JavaScript에서는 정확한 20자리 이상의 연산을 지원하지 않으므로 결과값이 지수표기법으로 나오도록 유지..ㅠ
-            } catch (error) {
-                display.textContent = 'Error';
-            }
-        } else {
-            if (display.textContent === '0') {
-                display.textContent = buttonText;
-            } else {
-                const newContent = display.textContent + buttonText;
-                
-                display.textContent = newContent;
-                updateFontSize();
-            }
-        }
+        handleButtonClick(buttonText);
     });
 });
 
+// '±' 버튼 클릭 이벤트 핸들러 등록
+negateButton.addEventListener('click', function() {
+    handleNegateButton();
+});
+
+// 버튼 클릭 처리 함수
+function handleButtonClick(buttonText) {
+    if (buttonText === 'C') {
+        handleClearButton();
+    } else if (buttonText === '=') {
+        handleEqualsButton();
+    } else {
+        handleNumberOrOperatorButton(buttonText);
+    }
+}
+
+// 숫자 또는 연산자 버튼 클릭 처리
+function handleNumberOrOperatorButton(buttonText) {
+    if (display.textContent === '0') {
+        display.textContent = buttonText;
+    } else {
+        const newContent = display.textContent + buttonText;
+        display.textContent = newContent;
+        updateFontSize();
+    }
+}
+
+// 등호(=) 버튼 클릭 처리
+function handleEqualsButton() {
+    try {
+        const expression = display.textContent.replace(/×/g, '*').replace(/÷/g, '/');
+        display.textContent = evaluate(expression);
+    } catch (error) {
+        display.textContent = 'Error';
+    }
+}
+
+// 클리어(C) 버튼 클릭 처리
+function handleClearButton() {
+    resetDisplayStyle();
+}
+
+// '±' 버튼 클릭 처리 함수
+function handleNegateButton() {
+    const currentValue = parseFloat(display.textContent);
+
+    if (!isNaN(currentValue)) {
+        // 현재 값이 숫자인 경우에만 양/음수를 전환
+        const newValue = -currentValue;
+        display.textContent = newValue.toString();
+        updateFontSize();
+    }
+}
+
+// 디스플레이 폰트 크기 업데이트
 function updateFontSize() {
     const currentFontSize = parseInt(window.getComputedStyle(display).fontSize);
     const maxCharacters = Math.floor((display.offsetWidth - 10) / (currentFontSize * 0.6)); 
@@ -41,8 +78,8 @@ function updateFontSize() {
     }
 }
 
+// 디스플레이 초기화
 function resetDisplayStyle() {
     display.textContent = '0';
     display.style.fontSize = '28px';
 }
-
